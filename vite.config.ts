ae45a18815766fb9a8317e880594a633c1d71863
Jeet Patel <jeet_21199@ldrp.ac.in>
2025-03-20 21:31:10 +0530

    some bug fix for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,10 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0', // Exposes the server to external connections
-    port: process.env.PORT || 4173, // Uses Render's PORT or defaults to 4173
+    host: true, // Allow external access
+    strictPort: true // Ensures the port is fixed
   },
   preview: {
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173,
-  },
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-  // base: '/'// Change this if your site is hosted in a subdirectory
+    allowedHosts: ["disease-surveillance-ai.onrender.com"]
+  }
 });
